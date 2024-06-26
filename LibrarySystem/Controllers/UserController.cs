06d3a748010f3cdf480a493ade6b54994ccb8816
sanjana <sanjana.bandara@cube.global>
2024-06-26 20:23:35 +0530

    feat():create user and get user

--- LibrarySystem/Controllers/UserController.cs
+++ LibrarySystem/Controllers/UserController.cs
@@ -1,5 +1,8 @@
-﻿using LibrarySystem.Model;
+﻿using LibrarySystem.DbContexts;
+using LibrarySystem.Entities;
+using LibrarySystem.Model;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibrarySystem.Controllers
 {
@@ -8,17 +11,40 @@ namespace LibrarySystem.Controllers
     public class UserController:ControllerBase
     {
 
-        [HttpGet]
-        public IActionResult Get()
+        private DataContext _dataContext;
+        public UserController(DataContext dataContext)
         {
-          return Ok();
+            _dataContext = dataContext;
+        }
+
+        [HttpGet("{userName}")]
+        public async Task<IActionResult> Get(string userName)
+        {
+        var user = await _dataContext.Users.FirstOrDefaultAsync(u => u.UserName == userName);
+            if (user == null) { 
+            return NotFound();
+            }
+          return Ok(user);
         }
 
 
         [HttpPost]
-        public IActionResult Create([FromBody] UserDetail user )
+        public async Task<IActionResult> Create([FromBody] UserDetail userDetail )
         {
+            if (userDetail == null) {
+                return BadRequest();
+            }
+            var user = new User();
+            user.UserName = userDetail.UserName;
+            user.Email = userDetail.Email;
+            user.Password = userDetail.Password;
+            user.UserType = 1;
+            _dataContext.Users.Add(user);
+            await _dataContext.SaveChangesAsync();
+
             return Ok(user);
+
+
         }
 
 
