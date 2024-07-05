5809100c1186d39e03bdc7e28033044fa3443421
Ganidu Abeysekara <ganiduofficial@gmail.com>
2024-07-05 16:06:23 +0530

    Local host issue fixing

--- LibrarySystem/Repository/Interface/IUserRepository.cs
+++ LibrarySystem/Repository/Interface/IUserRepository.cs
@@ -5,7 +5,6 @@ namespace LibrarySystem.Repository.Interface
     public interface IUserRepository
     {
         public bool SaveChangers();
-        public  void SaveChangesAsync();
         public void AddEntity<T>(T entityToAdd);
         public void RemoveEntity<T>(T entityToAdd);
         public User GetUserByEmail(string userName);
